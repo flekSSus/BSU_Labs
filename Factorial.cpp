3b37185bcf93415809343d46b52f16ba743b8403
Vova <zayac.zayac.w@gmail.com>
2024-06-26 16:52:10 +0300

    Release v1

---
+++ Factorial.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()      //Факториал
+{
+    unsigned long long int f=1;
+    int value;
+    cout<< "Enter value to get its factorial: ";
+    cin>> value;
+    while(value<0)
+    {
+        cout << "\nExeption! There is no solution!\n";
+        cin >> value;
+    }
+    if (value == 0)
+    {
+        cout<< value << "! = " << f;
+        return 1;
+    }
+    for (int i = value;i > 0;i--)
+    {
+        f = f * i;
+    }
+    cout<< value << "! = " << f;
+    return 1;
+}
+
+
